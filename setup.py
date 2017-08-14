3c8bd6251b3c81925c891d3ad4e5f58ef96d9ef3
Graham Krizek <gkrizek@nodesource.com>
2017-08-14 10:03:40 -0600

    make readme long_description

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
 setup(
     name="leh",
     version="0.1.0",
     description="Lambda Exception Helper",
+    long_description=readme(),
     author="Graham Krizek",
     author_email="graham@krizek.io",
     url="https://github.com/gkrizek/leh",
